f894696047945a943ad6299a2a4005ca113043ff
ZhaoWorkStation <dungbuit1k28@gmail.com>
2024-12-10 15:31:14 +0700

    Update num gen once

--- datalabel/label_mbpp_instruct.py
+++ datalabel/label_mbpp_instruct.py
@@ -76,11 +76,12 @@ import os
 from benchmark.MBPP.human_eval.evaluation import evaluate_functional_correctness_each_sample
 
 data_root = "/drive2/tuandung/WCODELLM/benchmark/MBPP/data"
-continue_from = '/drive2/tuandung/WCODELLM/jaist/codellama/LFCLF_embedding_mbpp_codellama_CodeLlama-7b-Instruct-hf_1.parquet'
+continue_from = '/drive2/tuandung/WCODELLM/jaist/magic_coder/LFCLF_embedding_mbpp_ise-uiuc_Magicoder-S-DS-6.7B_1.parquet'
 kwargs_handlers = [DistributedDataParallelKwargs(find_unused_parameters=True)]
 accelerator = Accelerator(mixed_precision="bf16", kwargs_handlers=kwargs_handlers)
-model_name = 'codellama/CodeLlama-7b-Instruct-hf'
+# model_name = 'codellama/CodeLlama-7b-Instruct-hf'
 # model_name = 'Qwen/Qwen2.5-Coder-3B-Instruct'
+model_name = 'ise-uiuc/Magicoder-S-DS-6.7B'
 tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
 # sequences = pd.read_pickle(continue_from)
 sequences = pd.read_parquet(continue_from).to_dict(orient='records')
--- pipeline/save_hidden_states.py
+++ pipeline/save_hidden_states.py
@@ -35,6 +35,7 @@ parser.add_argument('--device', type=str, default='cuda:0')
 parser.add_argument('--tensor_parallel_size', type=int, default=1)
 parser.add_argument('--fraction_of_data_to_use', type=float, default=1.0)
 parser.add_argument('--num_generations_per_prompt', type=int, default=10)
+parser.add_argument('--max_num_gen_once', type=int, default=2)
 parser.add_argument('--max_new_tokens', type=int, default=500)
 parser.add_argument('--temperature', type=float, default=0.5)
 parser.add_argument('--decoding_method', type=str, default='greedy')
@@ -89,7 +90,7 @@ def get_dataset_fn(data_name):
 
 
 @torch.no_grad()
-def get_generations(model_name:str, args, seed=1, old_sequences=None, max_num_gen_once=args.num_generations_per_prompt,cache_dir='output'):
+def get_generations(model_name:str, args, seed=1, old_sequences=None, max_num_gen_once=args.max_num_gen_once,cache_dir='output'):
     device = args.device
     model, tokenizer = models.load_model_and_tokenizer(model_name, args.device, args.load_in_8bit)    
     utils.seed_everything(seed)
@@ -137,6 +138,8 @@ def get_generations(model_name:str, args, seed=1, old_sequences=None, max_num_ge
         generations_decoded = []
         # print("Prompt:", tokenizer.decode(input_ids.cpu()[0], skip_special_tokens=True))
         num_gens = args.num_generations_per_prompt
+        all_token_hidden_states_layer_list = {}
+        off_set = 0
         while num_gens > 0:
             dict_outputs =  model.generate(input_ids, attention_mask=batch['attention_mask'].to(device),
                             num_beams=1, max_new_tokens=args.max_new_tokens, num_return_sequences=min(max_num_gen_once, num_gens),
@@ -157,14 +160,17 @@ def get_generations(model_name:str, args, seed=1, old_sequences=None, max_num_ge
             layers_to_process = args.layers
             hidden_states = dict_outputs.hidden_states
             ###### hidden_states : (num_tokens, num_layers, num_seq, num_input_tokens/1, embedding_size)
-            all_token_hidden_states_layer_list = {}
+            
             for layer in layers_to_process:
                 all_token_hidden_states_layer = {}
                 for ind in range(hidden_states[1][-1].shape[0]):
-                    all_token_hidden_states_layer[ind] = []
+                    all_token_hidden_states_layer[ind + off_set] = []
                     for hidden_state in hidden_states[1:]:
-                        all_token_hidden_states_layer[ind].append(hidden_state[layer][ind, -1, :].detach().cpu().float().numpy())
-                all_token_hidden_states_layer_list[layer] = all_token_hidden_states_layer
+                        all_token_hidden_states_layer[ind + off_set].append(hidden_state[layer][ind, -1, :].detach().cpu().float().numpy())
+
+                if layer not in all_token_hidden_states_layer_list:
+                    all_token_hidden_states_layer_list[layer] = {}
+                all_token_hidden_states_layer_list[layer].update(all_token_hidden_states_layer)
             # return hidden_state
             
             for gen_ids in generations:
@@ -178,7 +184,8 @@ def get_generations(model_name:str, args, seed=1, old_sequences=None, max_num_ge
             gc.collect()
             torch.cuda.empty_cache()
             num_gens -= len(generation)
-        
+            off_set += len(generation)
+            
         for layer in layers_to_process:
             layer_embeddings = all_token_hidden_states_layer_list[layer]
             layer_embeddings_dict = dict(
